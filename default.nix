83ba7e37a4260c903c6b5199092d6f833f96642d
Sönke Hahn <soenke.hahn@zalora.com>
2014-10-29 16:22:05 +0800

    nix: filter src

--- default.nix
+++ default.nix
@@ -1,5 +1,10 @@
 { pkgs ? import <nixpkgs> {},
-  src ? ./. } :
+  src ? builtins.filterSource (path: type:
+    type != "unknown" &&
+    baseNameOf path != ".git" &&
+    baseNameOf path != "result" &&
+    baseNameOf path != "dist") ./.
+} :
 {
     build = pkgs.haskellPackages.buildLocalCabal src "journal-mailer";
 }
